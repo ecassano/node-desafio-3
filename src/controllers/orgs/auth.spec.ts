0850c7e98094dc170e8681d3e5c9bec62fc2aaa9
eduardo.sa <eduardo.sa@rio.rj.gov.br>
2025-08-11 14:30:09 -0300

    feat: Add all orgs routes its e2e tests

---
+++ src/controllers/orgs/auth.spec.ts
@@ -0,0 +1,39 @@
+import { app } from "@/app";
+import { afterAll, beforeAll, describe, expect, it } from "vitest";
+import request from "supertest";
+
+describe("Auth", () => {
+  beforeAll(async () => {
+    await app.ready();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it("should be able to authenticate an organization", async () => {
+    await request(app.server).post("/orgs").send({
+      name: "Test Organization",
+      accountable_name: "Test Accountable",
+      email: "test@test.com",
+      whatsapp: "1234567890",
+      password: "123456",
+      cep: "1234567890",
+      state: "Test State",
+      city: "Test City",
+      neighborhood: "Test Neighborhood",
+      street: "Test Street",
+      complement: "Test Complement",
+    });
+
+    const response = await request(app.server).post("/sessions").send({
+      email: "test@test.com",
+      password: "123456",
+    });
+
+    console.log(response.body);
+
+    expect(response.status).toBe(200);
+    expect(response.body.token).toEqual(expect.any(String));
+  });
+});
---
+++ src/controllers/orgs/profile.spec.ts
@@ -0,0 +1,31 @@
+import { afterAll, beforeAll, describe, it, expect } from "vitest";
+import { app } from "@/app";
+import { createAndAuthOrg } from "@/utils/test/create-and-auth-org";
+import request from "supertest";
+
+describe("Profile", () => {
+  beforeAll(async () => {
+    await app.ready();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it("should be able to get an organization's profile", async () => {
+    const { token } = await createAndAuthOrg(app);
+
+    const response = await request(app.server)
+      .get("/me")
+      .set("Authorization", `Bearer ${token}`)
+      .send();
+
+    expect(response.status).toBe(200);
+    expect(response.body.org).toEqual(
+      expect.objectContaining({
+        name: "Test Organization",
+        email: "test@test.com",
+      })
+    );
+  });
+});
